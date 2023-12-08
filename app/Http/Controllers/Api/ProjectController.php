a4fc2cc8663f10d7bdb5f27562bb1a1763593b94
AnotherDebug <antonio.faddanno@gmail.com>
2023-12-08 10:36:17 +0100

    [ADD]: ProjectController api [ADD]: route getProject

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,9 +8,27 @@ use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $projects = Project::with('type', 'technologies')->get();
 
         return response()->json($projects);
+    }
+
+    public function getProject($id)
+    {
+        $project = Project::where('id', $id)->with('type', 'technologies')->first();
+
+        if ($project['image']) {
+
+            $project['image'] = asset('storage/' . $project->image);
+
+        } else {
+
+            $project['image'] = 'https://via.placeholder.com/200x200';
+
+        }
+
+        return response()->json($project);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use App\Http\Controllers\Api\ProjectController;
 */
 
 Route::get('/projects', [ProjectController::class, 'index']);
+
+Route::get('/project/{id}', [ProjectController::class, 'getProject']);
